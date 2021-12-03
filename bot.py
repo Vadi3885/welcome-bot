7882c802fbe19af33579463ab7c2516231627ee7
Vadi3885 <95479114+Vadi3885@users.noreply.github.com>
2021-12-03 20:05:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ def help(updater,context):
 
 def add_group(update: Update, context: CallbackContext):
     for member in update.message.new_chat_members:
-        update.message.reply_text(f'Hello {member.full_name} , Welcome to Drivers welfare help line Thank you for Joining  ')
+        update.message.reply_text(f'Hello {member.full_name} , Welcome to 🙏 DRIVERS WELFARE HELPLINE 🙏 Thank you for Joining  ')
 
 add_group_handle = MessageHandler(Filters.status_update.new_chat_members, add_group)
 updater.dispatcher.add_handler(add_group_handle)
